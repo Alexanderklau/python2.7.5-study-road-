d441581a45831953ea1a47837e668a15e09109ef
Alexanderlau <429095816@qq.com>
2016-08-05 11:17:19 +0800

    20160805

---
+++ 040.py
@@ -0,0 +1,12 @@
+def lookup(data,label,name):
+    return data[label].get(name)
+def store(data,full_name):
+    names = full_name.split()
+    if len(names) == 2:names.insert(1,'')
+    labels = 'First','middle','last'
+    for label,name in zip(labels,names):
+        people = lookup(data,label,name)
+        if people:
+            people.append(full_name)
+        else:
+            data[label][name] = [full_name]
