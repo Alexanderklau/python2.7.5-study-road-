281615757b2d10f3f94a4e2c8a250db3a2ec3892
Alexanderlau <429095816@qq.com>
2016-08-22 14:27:26 +0800

    2016

---
+++ 101.py
@@ -0,0 +1,9 @@
+x = raw_input('Enter a number!')
+while x > 1:
+    if x % 2 == 0:
+        print 'The number  %d is Even' % x
+    else:
+        print 'The number  %d is obb' % x
+        break
+        
+
